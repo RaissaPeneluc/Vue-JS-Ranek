7975406f7d89794c932afe0fc5582a75171b6fe6
Raíssa Peneluc <raissapeneluc@gmail.com>
2025-03-19 11:58:34 -0300

    :sparkles: FEAT: Criando componentes de Header e Footer.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,89 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-    </nav>
-    <router-view/>
+    <TheHeader />
+    <main id="main">
+      <router-view />
+    </main>
+    <TheFooter />
   </div>
 </template>
 
-<style>
+<script>
+import TheFooter from "./components/TheFooter.vue";
+import TheHeader from "./components/TheHeader.vue";
 
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+
+<style>
+/* Estilos Gerais */
+
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: #345;
+  background: url("./assets/pattern.svg");
+}
+
+ul {
+  list-style: none;
+}
+
+a {
+  color: #345;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 10px 30px;
+  background: #87f;
+  border-radius: 4px;
+  color: white;
+  text-align: center;
+  font-size: 1rem;
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1); /* Fazendo o botão crescer ao passar o cursor. */
+}
+ 
+#app { /* Estilização pro footer sempre ficar no final. */ 
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+
+#main {
+  flex: 1; /* Se expande e se o conteúdo crescer, o footer continua em baixo. */
+}
 </style>
