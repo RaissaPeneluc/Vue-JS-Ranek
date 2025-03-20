26c1015f634610aa5de6831b94c749a8f579b194
Raíssa Peneluc <raissapeneluc@gmail.com>
2025-03-20 16:38:58 -0300

    :sparkles: FEAT: Criando componentes e API local dos produtos e usuários.

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="products-container">
+    <div v-for="product in products" :key="product.id">
+      <img
+        v-if="product.fotos"
+        :src="product.fotos[0].src"
+        alt="product.fotos[0].titulo"
+      />
+      <p class="price">{{ product.preco }}</p>
+      <h2 class="title">{{ product.nome }}</h2>
+      <p class="description">{{ product.descricao }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: null,
+    };
+  },
+  methods: {
+    getProducts() {
+      fetch("http://localhost:3000/produto")
+        .then((r) => r.json())
+        .then((r) => {
+          this.products = r;
+        });
+    },
+  },
+  created() {
+    this.getProducts();
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="home">
-   
-  </div>
+  <section>
+    <h1>Compre ou Venda</h1>
+    <SearchProduct />
+    <ProductsList />
+  </section>
 </template>
 
 <script>
+import ProductsList from '@/components/ProductsList.vue';
+import SearchProduct from '@/components/SearchProduct.vue';
+
 export default {
   name: 'HomeView',
   components: {
-    
+    ProductsList,
+    SearchProduct
   }
 }
 </script>
+
+<style scoped>
+h1{
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
